41bce3dbe6e4d2606ef5631312253609357f1b4a
jst1994 <jst_1994@qq.com>
2015-06-09 07:33:49 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("*\n"
+          "**\n"
+          "***\n");
+   printf(  "       *        \n"
+            "  *		*    \n"
+            "*			  *\n"
+            "  *		*    \n"
+            "       *        \n");
+    printf("						*\n"
+           "					*\n"
+           "				*\n"
+           "*			*\n"
+           "	*	*\n"
+           "		*\n");
+
+}
